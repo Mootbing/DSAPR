e98986e95ab61f270af3c7a677a954ee7b516d09
Mootbing <50122069+Mootbing@users.noreply.github.com>
2022-07-21 00:25:24 -0400

    wip

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -2,7 +2,7 @@ import { MDBBtn, MDBIcon, MDBTextArea, MDBContainer, MDBDatatable, MDBModal, MDB
 import { useEffect, useState } from "react";
 import { useElapsedTime } from "use-elapsed-time";
 
-function checkBlast({ id, setResult }) {
+function checkBlast({ id, setResult, aftermath }) {
     if (id == undefined) {
         return;
     }
@@ -22,24 +22,69 @@ function checkBlast({ id, setResult }) {
                         data = parser.parseFromString(data, "text/xml");
                         data = data.getElementsByTagName("Hit");
                         setResult(Array.from(data));
+                        if (aftermath != null) {
+                            setTimeout(aftermath, 30000);
+                        }
                     }
                     );
             }
             else {
-                setTimeout(() => checkBlast({ id }), 10000);
+                setTimeout(() => checkBlast({ id, setResult, aftermath }), 30000);
             }
         });
 }
 
-function runBLAST({ db = "nt", program = "blastn", qDNA, setResult }) {
+function runBLAST({ db = "nt", program = "blastn", qDNA, setResult, aftermath }) {
     fetch("https://blast.ncbi.nlm.nih.gov/Blast.cgi?PROGRAM=" + program + "&DATABASE=" + db + "&QUERY=" + qDNA + "&CMD=Put")
         .then(data => data.text())
         .then(data => {
             const [id, seconds] = data.match("QBlastInfoBegin(.|\n)*?(QBlastInfoEnd)")[0].split("\n").slice(1, -1).map(value => value.replace("RID = ", "").replace("RTOE = ", "").replace("\s/g", ""));
-            console.log(id)
-            console.log(seconds)
-            checkBlast({ id, setResult })
+            console.log("id: " + id);
+            checkBlast({id, setResult, aftermath})
         });
+}
+
+function BLASTTables({rows, blastSrc, cols = [
+    "Accession #",
+    "Description", 
+    "E-Value", 
+    "Start",
+    "End",
+    "Frame"
+]}){
+
+    if (blastSrc != null){
+        rows = blastSrc.map(hit => {
+            const HSP = hit.childNodes[11].childNodes[1];
+            return [
+                hit.childNodes[7].textContent,
+                hit.childNodes[5].textContent, 
+                HSP.childNodes[7].textContent.includes("e") ? (hit.childNodes[11].childNodes[1].childNodes[7].textContent.split(".")[0] + "e-" + hit.childNodes[11].childNodes[1].childNodes[7].textContent.split("-")[1]) : hit.childNodes[11].childNodes[1].childNodes[7].textContent, 
+                HSP.childNodes[9].textContent,
+                HSP.childNodes[11].textContent,
+                HSP.childNodes[17].textContent,
+            ]}
+        )
+    }
+
+    if (rows == null)
+    {
+        rows = [];
+    }
+
+    return (
+        <MDBDatatable
+            entries={10}
+            search
+            entriesOptions={[10]}
+            data={{
+                columns: cols,
+                rows: rows
+            }}
+            style={{ backgroundColor: "#202020", color: "#fff" }}
+            dark
+        />
+    );
 }
 
 export default function Results({ closeBtn }) {
@@ -49,13 +94,27 @@ export default function Results({ closeBtn }) {
     const { elapsedTime } = useElapsedTime({ isPlaying: true });
 
     const [blastn_nrnt, setBlastn_nrnt] = useState(null);
+    const [blastn_est, setBlastn_est] = useState(null);
+    const [blastx_nrnt, setBlastx_nrnt] = useState(null);
+
     const loading = blastn_nrnt == null;
 
     useEffect(() => {
+
+        const blastN_2 = () => {
+            runBLAST({db: "est", program: "blastn", qDNA: DNASequence, setResult: setBlastn_est, aftermath: blastX});
+        }
+
+        const blastX = () => {
+            console.log("All done!")
+        }
+
         console.log(DNASequence + " from results");
-        // runBLAST({db: "nt", program: "blastn", qDNA: DNASequence, setResult: setBlastn_nrnt});
-        checkBlast({ id: "DH3FE7AP016", setResult: setBlastn_nrnt });
+        runBLAST({db: "nt", program: "blastn", qDNA: DNASequence, setResult: setBlastn_nrnt, aftermath: blastN_2});
+        // checkBlast({id: "DHK9U13K01R", setResult: setBlastn_nrnt});
     }, [])
+
+    const games = ["http://slither.io/", "https://moomoo.io", "https://youtube.com"];
 
     return (
         <MDBModal show staticBackdrop>
@@ -65,14 +124,14 @@ export default function Results({ closeBtn }) {
                         <MDBModalTitle>
                             {loading ? "BLASTing..." : "BLAST Results"}
                         </MDBModalTitle>
-                        <MDBBtn onClick={closeBtn} className="btn-close" color="white" />
+                        {!loading && <MDBBtn onClick={closeBtn} className="btn-close" color="white" />}
                     </MDBModalHeader>
                     {loading ? <MDBModalBody>
                         <center>
                             <MDBSpinner color="white" className="mb-4 mt-4" />
                             <p className="mb-0">{(new Date(Number(String(elapsedTime).split(".")[0]) * 1000)).toISOString().slice(14, 19)} - It may take a while to run all the BLAST searches required.</p>
                             <p className="sub-p">Please leave this page open - it will automatically refresh when done</p>
-                            <iframe className="pt-4" src="http://slither.io/" width="90%" height="450px" />
+                            <iframe className="pt-4" src={games[0]} width="90%" height="450px" />
                             <p className="sub-p">The game will disconnect when done.</p>
                         </center>
                     </MDBModalBody> :
@@ -85,47 +144,10 @@ export default function Results({ closeBtn }) {
                                 value={DNASequence}
                                 style={{ border: "0px solid black", backgroundColor: "rgba(50, 50, 50, 50)", color: "#fff", borderRadius: "3px", minWidth: "100%", paddingLeft: "10px", minHeight: "100px" }}
                             />
-                            <h5>BLASTn</h5>
-                            <MDBDatatable
-                                entries={10}
-                                search
-                                entriesOptions={[10]}
-                                data={{
-                                    columns: 
-                                    [
-                                        // [{ 
-                                        //     label: 'Organism Name / Description', 
-                                        //     field: "org", 
-                                        //     width: 500 
-                                        // }, { 
-                                        //     label: 'E-Value', 
-                                        //     field: "e" 
-                                        // }, { 
-                                        //     label: 'Accession #', 
-                                        //     field: "a" 
-                                        // }]
-
-                                        ["Organism Name / Description", "E-Value", "Accession #"]
-                                    ],
-                                
-                                    rows: blastn_nrnt.map(hit => [
-                                            hit.childNodes[5].textContent, 
-                                            hit.childNodes[11].childNodes[1].childNodes[7].textContent, 
-                                            hit.childNodes[7].textContent 
-                                        ]
-                                    )
-                                }}
-                                // data={{
-                                //     columns: [
-                                //         ["a"]
-                                //     ],
-                                //     row: [
-                                //         ["b"]
-                                //     ]
-                                // }}
-                                style={{ backgroundColor: "#202020", color: "#fff" }}
-                                dark
-                            />
+                            <h5>BLASTn NRNT</h5>
+                            <BLASTTables blastSrc={blastn_nrnt}/>
+                            <h5>BLASTn EST</h5>
+                            <BLASTTables blastSrc={blastn_est}/>
                             <MDBRow>
                                 <MDBCol>
                                     <h5>Protein Sequence</h5>
@@ -153,28 +175,10 @@ export default function Results({ closeBtn }) {
                                 value="replace with protein seq"
                                 style={{ border: "0px solid black", backgroundColor: "rgba(50, 50, 50, 50)", color: "#fff", borderRadius: "3px", minWidth: "100%", paddingLeft: "10px", minHeight: "100px" }}
                             />
-                            <h5>BLASTx</h5>
-                            <MDBDatatable
-                                data={{
-                                    columns: ['Organism Name', 'Description', 'E-Value', 'Accession #', 'Link'],
-                                    rows: [
-                                        ['Tiger Nixon', 'System Architect', 'Edinburgh', '61', '2011/04/25', '$320,800'],
-                                    ]
-                                }}
-                                style={{ backgroundColor: "#202020", color: "#fff" }}
-                                dark
-                            />
+                            {/* <h5>BLASTx</h5>
+                            <BLASTTables blastSrc={blastx_nrnt} />
                             <h5>BLASTp</h5>
-                            <MDBDatatable
-                                data={{
-                                    columns: ['Organism Name', 'Description', 'E-Value', 'Accession #', 'Link'],
-                                    rows: [
-                                        ['Tiger Nixon', 'System Architect', 'Edinburgh', '61', '2011/04/25', '$320,800'],
-                                    ]
-                                }}
-                                style={{ backgroundColor: "#202020", color: "#fff" }}
-                                dark
-                            />
+                            <BLASTTables blastSrc={blastx_nrnt} /> */}
                         </MDBModalBody>}
                 </MDBModalContent>
             </MDBModalDialog>
