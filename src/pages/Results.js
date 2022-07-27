a410fcc12a72bbe0771f1a13fb9671f45ac47555
Mootbing <50122069+Mootbing@users.noreply.github.com>
2022-07-27 16:53:44 -0400

    aradopsis homolog works

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -38,12 +38,12 @@ function checkBlast({ id, setResult, aftermath }) {
         });
 }
 
-function runBLAST({ db = "nt", program = "blastn", qSeqeuence, setResult, next, aftermath, additionalParams="" }) {
+function runBLAST({ db = "nt", program = "blastn", qSequence, setResult, next, aftermath, additionalParams="" }) {
 
-    // console.log("https://blast.ncbi.nlm.nih.gov/Blast.cgi?PROGRAM=" + program + "&DATABASE=" + db + "&QUERY=" + qSeqeuence + "&CMD=Put" + additionalParams)
+    // console.log("https://blast.ncbi.nlm.nih.gov/Blast.cgi?PROGRAM=" + program + "&DATABASE=" + db + "&QUERY=" + qSequence + "&CMD=Put" + additionalParams)
     // return;
 
-    fetch("https://blast.ncbi.nlm.nih.gov/Blast.cgi?PROGRAM=" + program + "&DATABASE=" + db + "&QUERY=" + qSeqeuence + "&CMD=Put" + additionalParams)
+    fetch("https://blast.ncbi.nlm.nih.gov/Blast.cgi?PROGRAM=" + program + "&DATABASE=" + db + "&QUERY=" + qSequence + "&CMD=Put" + additionalParams)
         .then(data => data.text())
         .then(data => {
             const [id, seconds] = data.match("QBlastInfoBegin(.|\n)*?(QBlastInfoEnd)")[0].split("\n").slice(1, -1).map(value => value.replace("RID = ", "").replace("RTOE = ", "").replace("\s/g", ""));
@@ -100,6 +100,36 @@ function BLASTTables({rows, blastSrc, cols = [
     );
 }
 
+function runPDB({qSequence, setResult, next, aftermath}){
+
+    console.log(qSequence);
+
+    if (qSequence == null){
+        return;
+    }
+
+    setResult("https://www.rcsb.org/search?request="+`%7B"query"%3A%7B"type"%3A"group"%2C"logical_operator"%3A"and"%2C"nodes"%3A%5B%7B"type"%3A"terminal"%2C"service"%3A"sequence"%2C"parameters"%3A%7B"evalue_cutoff"%3A0.1%2C"identity_cutoff"%3A0%2C"target"%3A"pdb_protein_sequence"%2C"value"%3A"` + qSequence + `"%7D%7D%5D%7D%2C"return_type"%3A"polymer_entity"%7D`);
+
+    if (next != null) {
+        setTimeout(next, 5000);
+    }
+
+    if (aftermath != null) {
+        aftermath();
+    }
+
+    return;
+
+    // const request = `https://search.rcsb.org/rcsbsearch/v2/query?json=%7B"query"%3A%7B"type"%3A"group"%2C"logical_operator"%3A"and"%2C"nodes"%3A%5B%7B"type"%3A"terminal"%2C"service"%3A"sequence"%2C"parameters"%3A%7B"evalue_cutoff"%3A0.1%2C"identity_cutoff"%3A0%2C"target"%3A"pdb_protein_sequence"%2C"value"%3A"` + qSequence + `"%7D%7D%5D%7D%2C"return_type"%3A"polymer_entity"%7D`;
+    // console.log(request)
+
+    // fetch(request)
+    //     .then(data => data.text())
+    //     .then(data => {
+    //         console.log(data);
+    //     })
+}
+
 export default function Results({ closeBtn }) {
 
     const DNASequence = document.getElementById("query-textarea-dna").value;
@@ -123,12 +153,16 @@ export default function Results({ closeBtn }) {
     const [openedX_nrnt, setOpenedX_nrnt] = useState(true);
     const [openedP_nrnt, setOpenedP_nrnt] = useState(true);
     const [openedP_homoSapiens, setOpenedP_homoSapiens] = useState(true);
+    const [openedPDB, setOpenedPDB] = useState(true);
+    const [openedLocus, setOpenedLocus] = useState(true);
 
     const [blastn_nrnt, setBlastn_nrnt] = useState(null);
     const [blastn_est, setBlastn_est] = useState(null);
     const [blastx_nrnt, setBlastx_nrnt] = useState(null);
     const [blastp_nrnt, setBlastp_nrnt] = useState(null);
     const [blastp_homoSapiens, setBlastp_homoSapiens] = useState(null);
+    const [PDBLink, setPDBLink] = useState(null);
+    const [locus, setLocus] = useState(null);
 
     const setMessageDone = (program) => {
         setLoadLog(loadLog => [...loadLog, "     " + new Date(Date.now()) + "-" + program + " - Done"]);
@@ -139,30 +173,37 @@ export default function Results({ closeBtn }) {
 
         const blastN_NRNT = () => {
             setLoadLog(loadLog => [...loadLog, new Date(Date.now()) +" - Initiated BlastN-NRNT"]);
-            runBLAST({db: "nt", program: "blastn", qSeqeuence: DNASequence, setResult: setBlastn_nrnt, additionalParams: "&FILTER=T", next: blastN_EST, aftermath: () => setMessageDone("BlastN-NRNT")});
+            runBLAST({db: "nt", program: "blastn", qSequence: DNASequence, setResult: setBlastn_nrnt, additionalParams: "&FILTER=T", next: blastN_EST, aftermath: () => setMessageDone("BlastN-NRNT")});
         }
 
         const blastN_EST = () => {
             setLoadLog(loadLog => [...loadLog, new Date(Date.now()) +" - Initiated BlastN-EST"]);
-            runBLAST({db: "est", program: "blastn", qSeqeuence: DNASequence, setResult: setBlastn_est, additionalParams: "&FILTER=T", next: blastX, aftermath: () => setMessageDone("BlastN-EST")});
+            runBLAST({db: "est", program: "blastn", qSequence: DNASequence, setResult: setBlastn_est, additionalParams: "&FILTER=T", next: blastX, aftermath: () => setMessageDone("BlastN-EST")});
         }
 
         const blastX = () => {
             setLoadLog(loadLog => [...loadLog, new Date(Date.now()) +" - Initiated BlastX"]);
-            runBLAST({db: "nr", program: "blastx", qSeqeuence: DNASequence, setResult: setBlastx_nrnt, additionalParams: "&FILTER=F", next: blastP, aftermath: () => setMessageDone("BlastX")});
+            runBLAST({db: "nr", program: "blastx", qSequence: DNASequence, setResult: setBlastx_nrnt, additionalParams: "&FILTER=F", next: blastP, aftermath: () => setMessageDone("BlastX")});
         }
 
         const blastP = () => {
             setLoadLog(loadLog => [...loadLog, new Date(Date.now()) +" - Initiated BlastP"]);
-            runBLAST({db: "nr", program: "blastp", qSeqeuence: ProteinSequence, setResult: setBlastp_nrnt, additionalParams: "&FILTER=F", next: blastP_homoSapiens, aftermath: () => setMessageDone("BlastP")});
+            runBLAST({db: "nr", program: "blastp", qSequence: ProteinSequence, setResult: setBlastp_nrnt, additionalParams: "&FILTER=F", next: blastP_homoSapiens, aftermath: () => setMessageDone("BlastP")});
         }
 
         const blastP_homoSapiens = () => {
             setLoadLog(loadLog => [...loadLog, new Date(Date.now()) +" - Initiated BlastP - Homo sapiens"]);
-            runBLAST({db: "nr", program: "blastp", qSeqeuence: ProteinSequence, setResult: setBlastp_homoSapiens, additionalParams: "&FILTER=F&EQ_MENU=Home%20sapiens%20%28taxid%3A9606%29"});
+            runBLAST({db: "nr", program: "blastp", qSequence: ProteinSequence, setResult: setBlastp_homoSapiens, additionalParams: "&FILTER=F&EQ_MENU=Home%20sapiens%20%28taxid%3A9606%29"});
         }
 
-        blastN_NRNT();
+        const searchPDB = () => {
+            setLoadLog(loadLog => [...loadLog, new Date(Date.now()) +" - Initiated PDB"]);
+            runPDB({qSequence: ProteinSequence, setResult: setPDBLink, aftermath: () => setMessageDone("PDB")});
+        }
+
+        searchPDB();
+
+        // blastN_NRNT();
 
         // checkBlast({id: "DYR5UPN6016", setResult: setBlastn_nrnt});
     }, [])
@@ -170,16 +211,16 @@ export default function Results({ closeBtn }) {
     //check for updates on status
     useEffect(() => {
 
-        if (blastn_nrnt == null || blastn_est == null || blastx_nrnt == null || blastp_nrnt == null || blastp_homoSapiens == null)
-        {
-            return;
-        }
+        // if (blastn_nrnt == null || blastn_est == null || blastx_nrnt == null || blastp_nrnt == null || blastp_homoSapiens == null || PDBLink == null) {
+        // {
+        //     return;
+        // }
 
         setMessageDone("BlastP-Homo Sapiens")
 
         setLoadLog(loadLog => [...loadLog, "----------All Done!----------"]);
         setLoading(false);
-    }, [blastn_nrnt, blastn_est, blastx_nrnt, blastp_nrnt, blastp_homoSapiens])
+    }, [blastn_nrnt, blastn_est, blastx_nrnt, blastp_nrnt, blastp_homoSapiens, PDBLink])
 
     return (
         <MDBModal show staticBackdrop>
@@ -284,11 +325,82 @@ export default function Results({ closeBtn }) {
                             <MDBCollapse show={openedP_homoSapiens}>
                                 <h4 style={{color: "#6a65fc"}}>BLASTp Homo Sapiens</h4>
                                 <BLASTTables blastSrc={blastp_homoSapiens} />
-                                <MDBBtn 
+                                {/* <MDBBtn 
                                     color="link"
                                     className="mt-1 mb-1"
                                     style={{color: "#6a65fc", backgroundColor: "#242424", width: "100%"}}
-                                >Open BlastX Page</MDBBtn>
+                                >Open BlastX Page</MDBBtn> */}
+                            </MDBCollapse>
+
+                            <MDBBtn onClick={() => setOpenedPDB(!openedPDB)}
+                                color="link"
+                                className="mt-1 mb-1"
+                                style={{color: "#6a65fc", backgroundColor: "#242424", width: "100%"}}
+                            >RCSB Protein Matches</MDBBtn>
+                            <MDBCollapse show={openedPDB}>
+                            <h4 style={{color: "#6a65fc"}}>RCSB PDB Protein Matches</h4>
+                                <iframe src={PDBLink} width="100%" height="700px" style={{borderRadius: "5px"}} />
+                                <MDBBtn href={PDBLink}
+                                    target="_blank"
+                                    color="link"
+                                    className="mt-1 mb-1"
+                                    style={{color: "#39C0ED", backgroundColor: "#242424", width: "100%"}}
+                                ><MDBIcon icon="external-link-alt" className="me-2"/>Open RCSB In New Tab</MDBBtn>
+                            </MDBCollapse>
+
+                            <MDBBtn onClick={() => setOpenedLocus(!openedLocus)}
+                                color="link"
+                                className="mt-1 mb-1"
+                                style={{color: "#6a65fc", backgroundColor: "#242424", width: "100%"}}
+                            >Arabidopsis Homologs &amp; Cell Localization</MDBBtn>
+                            <MDBCollapse show={openedLocus}>
+
+                            <h4 style={{color: "#6a65fc"}}>Arabidopsis Homologs &amp; Cell Localization</h4>
+                                <MDBRow>
+                                    <MDBCol size="10">
+                                        <textarea
+                                            id="query-textarea-locus"
+                                            placeholder="Enter locus AT of homolog"
+                                            style={{ border: "0px solid black", backgroundColor: "#2d2d2d", color: "#fff", borderRadius: "3px", minWidth: "100%", paddingLeft: "10px", height: "35px" }}
+                                        />
+                                    </MDBCol>
+                                    <MDBCol size="2" className="ms-0">
+                                        <MDBBtn onClick={() => {
+                                            const loc = document.getElementById("query-textarea-locus").value;
+
+                                            if (loc.replace(" ", "") == "")
+                                            {
+                                                return;
+                                            }
+
+                                            setLocus(loc);
+
+                                            setOpenedLocus(false);
+                                            setTimeout(() => {setOpenedLocus(true)}, 500);
+
+                                        }}
+                                            color="link"
+                                            style={{color: "#39C0ED", backgroundColor: "#2d2d2d", width: "100%", height: "35px"}}
+                                        >Search!</MDBBtn>
+                                    </MDBCol>
+                                </MDBRow>
+                            <MDBCollapse show={locus != null}>
+                                {/* <iframe src={"http://bar.utoronto.ca/efp/cgi-bin/efpWeb.cgi?primaryGene="+locus+"&mode=Absolute&dataSource=Developmental_Map"} width="100%" height="700px" style={{borderRadius: "5px"}} /> */}
+                                <MDBBtn href={"http://bar.utoronto.ca/efp/cgi-bin/efpWeb.cgi?primaryGene="+locus+"&mode=Absolute&dataSource=Developmental_Map"}
+                                    target="_blank"
+                                    color="link"
+                                    className="mt-1 mb-1"
+                                    style={{color: "#39C0ED", backgroundColor: "#242424", width: "100%"}}
+                                ><MDBIcon icon="external-link-alt" className="me-2"/>Open Arabidopsis Homolog In New Tab</MDBBtn>
+
+                                {/* <iframe src={"http://bar.utoronto.ca/cell_efp/cgi-bin/cell_efp.cgi?primaryGene="+locus} width="100%" height="700px" style={{borderRadius: "5px"}} /> */}
+                                <MDBBtn href={"http://bar.utoronto.ca/cell_efp/cgi-bin/cell_efp.cgi?primaryGene="+locus}
+                                        target="_blank"
+                                        color="link"
+                                        className="mt-1 mb-1"
+                                        style={{color: "#39C0ED", backgroundColor: "#242424", width: "100%"}}
+                                    ><MDBIcon icon="external-link-alt" className="me-2"/>Open Cell Localization In New Tab</MDBBtn>
+                                </MDBCollapse>
                             </MDBCollapse>
                         </>}
                         <center>
